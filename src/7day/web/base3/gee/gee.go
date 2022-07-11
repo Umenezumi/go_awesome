fc8f6ded0351239ee565b067f67f63a4dd10c353
Umenezumi <pg.jianghao@gmail.com>
2022-07-11 17:18:00 +0800

    feat(7days): context impl of gee. complete day 2

--- src/7day/web/base3/gee/gee.go
+++ src/7day/web/base3/gee/gee.go
@@ -1,33 +1,23 @@
 package gee
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(ctx *Context)
 
 type Engine struct {
-	router map[string]HandlerFunc
+	*router
 }
 
 func (e *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	key := request.Method + "-" + request.URL.Path
-	if handler, ok := e.router[key]; ok {
-		handler(writer, request)
-	} else {
-		fmt.Fprintf(writer, "404 NOT FOUND: %s\n", request.URL)
-	}
+	c := newContext(writer, request)
+	e.handle(c)
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
-}
-
-func (e *Engine) addRouter(method, pattern string, handler HandlerFunc) {
-	key := fmt.Sprintf("%s-%s", method, pattern)
-	e.router[key] = handler
+	return &Engine{router: newRouter()}
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc) {
@@ -38,6 +28,6 @@ func (e *Engine) POST(pattern string, handler HandlerFunc) {
 	e.addRouter("POST", pattern, handler)
 }
 
-func (e *Engine) RUN(addr string) {
+func (e *Engine) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, e))
 }
